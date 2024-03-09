380b6c137945902632afcf902904ff53eb474c91
dtiradob <tiradoclf@gmail.com>
2024-03-09 16:41:22 -0300

    add preset and coreo

--- osclinga_3_3/osclinga_3_3.ino
+++ osclinga_3_3/osclinga_3_3.ino
@@ -51,6 +51,7 @@ int pasosCSV = 0;
 int pasoActual = 0;
 int waitingCSV = 0;
 unsigned long tiempoCSV = 0;
+String presets[] = {"P0", "P1", "P2", "P3", "P4", "P5", "P6", "P7" };
 
 AsyncWebServer server(80);
 
@@ -217,6 +218,8 @@ void setup() {
   server.on("/", HTTP_GET, handleRoot);
   server.on("/save_csv", HTTP_POST, handleSaveCSV);
   server.on("/get_csv", HTTP_GET, handleGetCSV);
+  server.on("/list_presets", HTTP_GET, handleListPresets);
+  server.on("/save_selections", HTTP_POST, handleSaveSelections);
   server.onNotFound(notFound);
   server.begin();
 
@@ -750,7 +753,18 @@ void displayFrecs() {
 
 //---------------WEB Y CSV FUNCTIONS-------------
 
+void handleSaveSelections(AsyncWebServerRequest *request) {
+  AsyncWebParameter* plainParam = request->getParam("plain", true);
+  if (plainParam != nullptr) {
+    String csvData = plainParam->value();
+    Serial.println("Received CSV data:");
+    Serial.println(csvData);
+    request->send(200, "text/plain", "Selections received successfully");
+  } else {
+    request->send(400, "text/plain", "Bad request: missing plain parameter");
+  }
 
+}
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
@@ -759,81 +773,98 @@ void notFound(AsyncWebServerRequest *request) {
 void handleRoot(AsyncWebServerRequest *request) {
   request->send(SPIFFS, "/index.html", "text/html");
 }
-
 void handleSaveCSV(AsyncWebServerRequest *request) {
-  Serial.println(request->params());
-  // Read the CSV data from the request body
-  String csvData = "";
-  AsyncWebParameter *p;
-  for (int i = 0; i < request->params(); i++) {
-    p = request->getParam(i);
-    int val = request->params() - 1;
-    if (i == val) {
-      csvData += p->value();
-    } else {
-      csvData += p->value() + "\n";
+  if (request->hasParam("name", true)) {
+    AsyncWebParameter* nameParam = request->getParam("name", true);
+    String fileName = "/" + nameParam->value() + ".csv";
+
+    Serial.println(request->params());
+    // Read the CSV data from the request body
+    String csvData = "";
+    AsyncWebParameter *p;
+    for (int i = 2; i < request->params(); i++) {
+      p = request->getParam(i);
+      int val = request->params() - 1;
+      if (i == val) {
+        csvData += p->value();
+      } else {
+        csvData += p->value() + "\n";
+      }
     }
-  }
 
 
-  if (!p->isPost()) {
-    request->send(400, "text/plain", "Bad request");
-    return;
-  }
+    if (!p->isPost()) {
+      request->send(400, "text/plain", "Bad request");
+      return;
+    }
 
-  // Open or create the file for writing
-  File file = SPIFFS.open("/form_data.csv", FILE_WRITE);
-  if (!file) {
-    request->send(500, "text/plain", "Error opening file for writing");
-    return;
-  }
+    // Open or create the file for writing
+    File file = SPIFFS.open(fileName, FILE_WRITE);
+    if (!file) {
+      request->send(500, "text/plain", "Error opening file for writing");
+      return;
+    }
 
-  // Write the CSV data to the file
-  if (file.print(csvData)) {
-    request->send(200, "text/plain", "CSV data saved successfully");
+    // Write the CSV data to the file
+    if (file.print(csvData)) {
+      request->send(200, "text/plain", "CSV data saved successfully");
+    } else {
+      request->send(500, "text/plain", "Error writing to file");
+    }
+    file.close();
+
+    // Re-open the file for reading
+    file = SPIFFS.open(fileName, FILE_READ);
+    if (!file) {
+      Serial.println("Error opening file for reading");
+      return;
+    }
+
+    // Print the content of the file
+    Serial.println("Contents of form_data.csv:");
+    while (file.available()) {
+      Serial.write(file.read());
+    }
+    Serial.println();
+
+    // Close the file again
+    file.close();
+    request->send(200, "text/plain", "CSV saved successfully");
   } else {
-    request->send(500, "text/plain", "Error writing to file");
-  }
-  file.close();
-
-  // Re-open the file for reading
-  file = SPIFFS.open("/form_data.csv", FILE_READ);
-  if (!file) {
-    Serial.println("Error opening file for reading");
-    return;
+    request->send(400, "text/plain", "Bad request: missing parameters");
   }
 
-  // Print the content of the file
-  Serial.println("Contents of form_data.csv:");
-  while (file.available()) {
-    Serial.write(file.read());
-  }
-  Serial.println();
 
-  // Close the file again
-  file.close();
 
   readCSV();
 }
 
 void handleGetCSV(AsyncWebServerRequest *request) {
-  // Open the CSV file for reading
-  File file = SPIFFS.open("/form_data.csv", FILE_READ);
-  if (!file) {
-    request->send(500, "text/plain", "Error opening file");
-    return;
+  if (request->hasParam("name")) {
+    AsyncWebParameter* nameParam = request->getParam("name");
+    String fileName = "/" + nameParam->value() + ".csv";
+    File file = SPIFFS.open(fileName, "r");
+    if (!file) {
+      request->send(404, "text/plain", "File not found");
+      return;
+    }
+    String csvContent = file.readString();
+    file.close();
+    request->send(200, "text/csv", csvContent);
+  } else {
+    request->send(400, "text/plain", "Bad request: missing parameters");
   }
-
-  // Read the content of the CSV file
-  String csvContent = "";
-  while (file.available()) {
-    csvContent += (char)file.read();
-  }
-  file.close();
-
-  // Send the CSV content to the client
-  request->send(200, "text/csv", csvContent);
 }
+
+void handleListPresets(AsyncWebServerRequest *request) {
+  String preset = "";
+  for (int i = 0; i < 8; i++) {
+    preset += presets[i] + ",";
+  }
+  // Send the CSV content to the client
+  request->send(200, "text/csv", preset);
+}
+
 
 void readCSV() {
   // Open the CSV file
